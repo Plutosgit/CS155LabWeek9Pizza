9c0b2e839f4446a4d6f0c4dc1a9f0db389d6e62d
Plutosgit <venkataraman_balaji@hotmail.com>
2021-10-24 00:00:02 -0700

    Add project files.

---
+++ CS155LabWeek9Pizza/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS155LabWeek9Pizza
+{
+    class Pizza
+    {
+        private string size;
+        private int numtoppings_cheese, numtoppings_pepperoni, numtoppings_ham;
+        private double cost;
+
+        //constructors
+        public Pizza()
+        {
+            size = "Unknown";
+            numtoppings_cheese = 0;
+            numtoppings_pepperoni = 0;
+            numtoppings_ham = 0;
+        }
+
+        public Pizza(string s, int c, int p, int h)
+        {
+            if (s.ToLower().Equals("small") || s.ToLower().Equals("medium") || s.ToLower().Equals("large"))
+            { 
+                size = s.ToLower();
+                numtoppings_cheese = c;
+                numtoppings_pepperoni = p;
+                numtoppings_ham = h;
+
+                CalculateCost();
+            }
+            else
+            {
+                Console.WriteLine("Invalid size. Pizza created with Unknown size!");
+                size = "Unknown";
+                numtoppings_cheese = 0;
+                numtoppings_pepperoni = 0;
+                numtoppings_ham = 0;
+            }
+        }
+
+        //public int Count { get => count; set => count = value; } //  was removed since problem statement says "There shoud be no input method or other setter methods. The only method that can set the counter is the one that sets it to 0."
+        public string Size { get => size; set => size = value.ToLower(); }
+        public int CheeseToppings { get => numtoppings_cheese; set => numtoppings_cheese = value; }
+        public int PepperoniToppings { get => numtoppings_pepperoni; set => numtoppings_pepperoni = value; }
+        public int HamToppings { get => numtoppings_ham; set => numtoppings_ham = value; }
+
+        public double CalculateCost()
+        {   
+            //calculate cost of toppings 
+            double costofToppings = 2.0 * (numtoppings_cheese + numtoppings_pepperoni + numtoppings_ham);
+
+            //First calculate cost by size of the pizza
+            switch (size)
+            {
+                case "small":
+                    cost = 10.0 + costofToppings;
+                    break;
+
+                case "medium":
+                    cost = 14.0 + costofToppings;
+                    break;
+
+                case "large":
+                    cost = 16.0 + costofToppings;
+                    break;
+
+                default:
+                    cost = 0.0;
+                    break;
+            }
+
+            return cost;
+        }
+
+        override public string  ToString()
+        {
+            string s = "Pizza:    Size = " + this.Size  +", Cost of Pizza = " + cost.ToString("C") + "\n" + "Cheese toppings = " + this.CheeseToppings + ", Pepperoni toppings = " + this.PepperoniToppings
+                + ", Ham toppings = " + this.HamToppings  + "\n";
+            return s;
+        }
+    }
+}
---
+++ CS155LabWeek9Pizza/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS155LabWeek9Pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Pizza objects for testers
+            Console.WriteLine("Hello! Lab - Week 9 - Pizza class");
+            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("Creating 2 pizza objects, 1 with default constructor and 1 with user constructor");
+            Console.WriteLine();
+
+            Pizza a = new Pizza();
+            Pizza b = new Pizza("medium", 2, 3, 4);
+
+            Console.WriteLine("Calculating cost for the 2 pizza objects");
+            Console.WriteLine("Cost of pizza A: $ " + a.CalculateCost().ToString());
+            Console.WriteLine("Cost of pizza B: $ " + b.CalculateCost().ToString());
+
+            Console.WriteLine(); 
+            Console.WriteLine("Showing Pizza A's overridden ToString method");
+            Console.WriteLine(a);
+            Console.WriteLine("Showing Pizza B's overridden ToString method");
+            Console.WriteLine(b);
+
+            Console.WriteLine();
+            Console.WriteLine("Changing Pizza A's size and toppings");
+            a.Size = "Large";   //L should work, since it will be converted to lowercase in the setter
+            a.CheeseToppings = 1;
+            a.PepperoniToppings = 2;
+            a.HamToppings = 3;
+            a.CalculateCost();
+            Console.WriteLine("Showing Pizza A after resetting its size & toppings");
+            Console.WriteLine(a);
+
+            Console.WriteLine();
+            Console.WriteLine("Trying to create a pizza with incorrect size 'X'..");
+            Pizza c = new Pizza("X", 1, 2, 3);
+            Console.WriteLine("\nEnd of testing pizzas!");
+
+            Console.Read();
+        }
+    }
+}
